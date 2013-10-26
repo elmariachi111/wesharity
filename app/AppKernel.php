c08f26629903499474f63154c10752548bed81fa
Stefan <stadolf@gmail.com>
2013-10-26 17:12:03 +0200

    baby steps: added layout file / base layout

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new TCHD\WesharityBundle\TCHDWesharityBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
